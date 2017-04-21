ac489214d15a35cae4261b0ebfe875bbb0084ce9
sfanari <s.fanari@web.de>
2017-04-21 17:00:18 +0200

    Unity Test Scene

---
+++ Programmierung/Toxicity/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    private Transform lookAT;
+    private Vector3 startOffset;
+
+	void Start ()
+    {
+        lookAT = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAT.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = lookAT.position + startOffset;
+	}
+}
